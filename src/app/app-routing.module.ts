640e0df5165814113bbe9e9ca85e8ded3fc15609
Mark Blandford <it@blandford.dev>
2023-11-05 08:09:06 +0000

    feat: reconfigure routes
    remove /home and move articles to child of articles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,11 +8,10 @@ import {
 } from './pages';
 
 const routes: Routes = [
-  { path: 'home', title: 'Blandford.dev - home', component: HomeComponent, data: [ { heading: 'Blandford.dev' } ] },
+  { path: '', title: 'Blandford.dev - home', component: HomeComponent, data: [ { heading: 'Blandford.dev' } ] },
   { path: 'articles', title: 'Blandford.dev - articles', component: ArticlesComponent, data: [ { heading: 'Articles' } ] },
-  { path: 'article/:id', component: RenderArticleComponent, data: [ { isArticle: true } ] },
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: '**', redirectTo: '/home', pathMatch: 'full' }
+  { path: 'articles/:id', component: RenderArticleComponent },
+  { path: '**', redirectTo: '/', pathMatch: 'full' }
 ];
 
 @NgModule({

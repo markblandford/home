48ccb12cf8a24fc0779eeb71c12564e1552771b8
Mark Blandford <8008207+markblandford@users.noreply.github.com>
2023-11-06 16:19:20 +0000

    feat: add netlify function to run universal server

---
+++ functions/server.js
@@ -0,0 +1,6 @@
+/* eslint-env es6 */
+const serverless = require('serverless-http');
+const server = require("../dist/home/server/main");
+
+module.exports = server.app;
+module.exports.handler = serverless(server.app);

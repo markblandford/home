e9daa80d93c1d5ea5c8a488a690dde7ef7bc4661
Mark Blandford <it@blandford.dev>
2023-11-17 12:11:20 +0000

    feat: update created dates

--- src/app/content/article-list.ts
+++ src/app/content/article-list.ts
@@ -18,8 +18,8 @@ export default class ArticleList {
       {
         id: 'how-i-practice-tdd',
         title: 'How I practice TDD',
-        createdDate: '2023-11-06',
-        updatedDate: '2023-11-06',
+        createdDate: '2023-11-18',
+        updatedDate: '2023-11-18',
         about: 'I enjoy Test Driven Development. I want more engineers to try it. This is how I do it: Test-driven development (TDD) is a software development process relying on software requirements being converted to test cases before software is fully developed, and tracking all software development by repeatedly testing the software against all test cases. This is as opposed to software being developed first and test cases created later.',
         tags: [
           'angular',

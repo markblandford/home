b9379e9036d53185746ec61ba408b4c9fcabb61a
Mark Blandford <it@blandford.dev>
2025-08-16 12:45:30 +0100

    docs: reduced the content in the threat model article

--- src/app/content/article-list.ts
+++ src/app/content/article-list.ts
@@ -55,7 +55,7 @@ export default class ArticleList {
         id: 'threat-modelling',
         title: 'Threat Modelling with Threat Dragon',
         createdDate: '2025-08-16',
-        updatedDate: '2025-08-17',
+        updatedDate: '2025-08-16',
         about: 'I often run threat modelling sessions with my team and few seem to pick it up. Here is my attempt at distilling the practice down to make it a little more approachable.',
         tags: [
           'security',
